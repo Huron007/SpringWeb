c604ca08c5c4f717745299fce08440bba7bfb363
Tomasz Marciniak <huron3572@gmail.com>
2022-05-10 11:33:01 +0200

    19.2 Zadanie

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -3,11 +3,17 @@ package com.crud.tasks;
 import com.crud.tasks.domain.TaskDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class TasksApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TasksApplication.class, args);
 	}
 }
